4908540d3c70b38998f8aa0351440a0d8b9566d1
nenad2024 <nenad2024@gmail.com>
2023-12-29 02:03:54 +0100

    Add project files.

---
+++ test2NenadLijeskic/EmployeeData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2NenadLijeskic
+{
+    public class EmployeeData
+    {
+        public string Id { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime StarTimeUtc { get; set; }
+        public DateTime EndTimeUtc { get; set; }
+        public string EntryNotes { get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public double TotalWorkedHours { get { return (EndTimeUtc - StarTimeUtc).TotalHours; } set { } }
+
+    }
+}
---
+++ test2NenadLijeskic/Program.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace test2NenadLijeskic
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string key = "vO17RnE8vuzXzPJo5eaLLjXjmRW07law99QTD90zat9FfOQJKKUcgQ==";
+            string api = $"https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code={key}";
+            HttpResponseMessage response = await client.GetAsync(api);
+            
+            string json = await response.Content.ReadAsStringAsync();
+                
+
+                
+             var employeeData = JsonConvert.DeserializeObject<List<EmployeeData>>(json);
+             var groupedResults = employeeData.GroupBy(
+             p => p.EmployeeName,
+             p => p.TotalWorkedHours,
+             (name, total) => new KeyValuePair<string, double>(name, total.Sum()));
+
+            var EmployeeNames = groupedResults.Select(x => x.Key).ToArray();
+            var EmployeeHours = groupedResults.Select(x => x.Value).ToArray();
+
+            string[] labels = EmployeeNames;
+            double[] data = EmployeeHours;
+
+            SKColor[] colors = new SKColor[data.Length];
+            Random random = new Random();
+            for (int j = 0; j < data.Length; j++)
+            {
+                colors[j] = new SKColor((byte)random.Next(0, 256), (byte)random.Next(0, 256), (byte)random.Next(0, 256));
+            }
+
+
+            int width = 600;
+            int height = 400;
+
+            using (SKBitmap bitmap = new SKBitmap(width, height))
+            {
+                
+                using (SKCanvas canvas = new SKCanvas(bitmap))
+                {
+                    canvas.Clear(SKColors.White); 
+
+                    using (SKPaint paint = new SKPaint())
+                    {
+                        SKRect rect = new SKRect(0, 0, width, height);
+                        double startAngle = 0;
+
+                        for (int i = 0; i < data.Length; i++)
+                        {
+                            paint.Color = colors[i]; 
+                            double sweepAngle = (360 * data[i]) / data.Sum(); 
+
+                            canvas.DrawArc(rect, (float)startAngle, (float)sweepAngle, true, paint);
+                            startAngle += sweepAngle; 
+                        }
+                    }
+
+                    Random rnd = new  Random();
+                    int r = rnd.Next();
+                    string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    string filePath = Path.Combine(desktopPath, $"PieChart{r}.png");
+
+
+
+                    using (FileStream fileStream = File.Create(filePath))
+                    {
+                        bitmap.Encode(SKEncodedImageFormat.Png, 100).SaveTo(fileStream);
+                        Console.WriteLine($"Pie chart download location: {filePath}");
+                    }
+                }
+            }
+        }
+    
+    }
+}
